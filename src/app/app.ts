53526144a227f38dbd370ac460d4c4971094c952
Abhishek-Kumar07 <a3@bmsit.in>
2026-09-01 12:31:02 +0530

    finished-header-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header';
+import { HeaderComponent } from './header/header';
 
 @Component({
   selector: 'app-root',
